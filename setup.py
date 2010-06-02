130b407e9fa2dadafd8baf27e86112a44ae8055c
Tres Seaver <tseaver@palladion.com>
2010-06-02 15:09:04 +0000

    Update project mailing list

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name='zope.xmlpickle',
       version = '3.5.0dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='XML-based Pickling',
       long_description=(
           read('README.txt')
